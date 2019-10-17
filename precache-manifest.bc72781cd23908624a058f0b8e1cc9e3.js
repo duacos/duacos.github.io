2c06ac7761565f3504f4aed41e9d093062844237
Jorge Durango <djorge.ivan97@gmail.com>
2019-10-16 21:33:25 -0500

    Updates

---
+++ precache-manifest.bc72781cd23908624a058f0b8e1cc9e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bf83a6734c82c12fa89f0591e1b7e1f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e0888dceef9b13fe557",
+    "url": "/static/css/2.383bd34b.chunk.css"
+  },
+  {
+    "revision": "f5a616b2cc4fee134b04",
+    "url": "/static/css/main.b9561ece.chunk.css"
+  },
+  {
+    "revision": "5e0888dceef9b13fe557",
+    "url": "/static/js/2.075440ed.chunk.js"
+  },
+  {
+    "revision": "f5a616b2cc4fee134b04",
+    "url": "/static/js/main.a7719f83.chunk.js"
+  },
+  {
+    "revision": "25293dcdd045d2663b65",
+    "url": "/static/js/runtime-main.67a84473.js"
+  },
+  {
+    "revision": "79379728b8d0383d9effd7cb05341878",
+    "url": "/static/media/menu.79379728.svg"
+  }
+]);
